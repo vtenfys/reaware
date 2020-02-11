def86d9b4a4192f7cf8341dd1de27edd10463d4e
David Bailey <davidbailey00@outlook.com>
2020-02-11 00:39:41 +0000

    Bundle node modules for production builds

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,21 @@
 import babel from "rollup-plugin-babel";
-import externals from "rollup-plugin-node-externals";
+import replace from "@rollup/plugin-replace";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
+import externals from "rollup-plugin-node-externals";
 import css from "rollup-plugin-css-porter";
 import html from "@rollup/plugin-html";
 
 import fs from "fs";
 import ejs from "ejs";
 import pkg from "./package.json";
+
+const find = id => require.resolve(id, { paths: ["node_modules/.pnpm"] });
+const namedExports = {
+  [find("react")]: ["Component", "createContext"],
+  [find("react-is")]: ["isValidElementType"],
+};
 
 export default {
   input: "src/App.js",
@@ -17,8 +26,12 @@ export default {
   external: ["nw.gui"],
   plugins: [
     babel(),
-    externals({ deps: true }),
-    process.env.NODE_ENV === "production" && terser(),
+    replace({ "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV) }),
+
+    ...(process.env.NODE_ENV === "production"
+      ? [resolve(), commonjs({ namedExports }), terser()]
+      : [externals({ deps: true })]),
+
     css({ raw: false }),
     html({
       title: pkg.window.title,
