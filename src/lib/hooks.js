5dee4a1f239cd0377f1acf0e8cac28649d278da9
David Bailey <davidbailey00@outlook.com>
2020-02-15 11:24:00 +0000

    Improve usePromise to work more reliably

--- src/lib/hooks.js
+++ src/lib/hooks.js
@@ -1,19 +1,21 @@
 export function usePromise(promise) {
   promise._status = promise._status ?? "pending";
 
-  const suspender = promise
-    .then(result => {
-      promise._status = "success";
-      promise._result = result;
-    })
-    .catch(error => {
-      promise._status = "error";
-      promise._result = error;
-    });
+  if (promise._status === "pending") {
+    const suspender = promise
+      .then(result => {
+        promise._status = "success";
+        promise._result = result;
+      })
+      .catch(error => {
+        promise._status = "error";
+        promise._result = error;
+      });
+
+    throw suspender;
+  }
 
   switch (promise._status) {
-    case "pending":
-      throw suspender;
     case "error":
       throw promise._result;
     default:
