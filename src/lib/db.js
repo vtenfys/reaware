ab3785d250c4aa0173c04ec4c2ee947511900593
David Bailey <davidbailey00@outlook.com>
2020-02-16 22:52:40 +0000

    Update to new indexeddb PouchDB adapter

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,4 +1,4 @@
 import PouchDB from "pouchdb";
-PouchDB.plugin(require("pouchdb-adapter-idb"));
+PouchDB.plugin(require("pouchdb-adapter-indexeddb"));
 
-export const db = new PouchDB("card_db", { adapter: "idb" });
+export const db = new PouchDB("card_db", { adapter: "indexeddb" });
